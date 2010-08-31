bd2df6210eb0d93dbdd24c6d30a0c7f6737b4e73
Aditya Sanghi <asanghi@me.com>
2010-09-01 00:09:31 +0530

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 #
 
 class User < ActiveRecord::Base 
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -19,5 +20,40 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, 
                     :format => {:with => email_regex}, 
                     :uniqueness => {:case_sensitive => false}
+  
+  validates :password, :confirmation => true,
+                      :presence => true,
+                      :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+  
+  def has_password?(sumbitted_password)
+    encrypt(sumbitted_password) == encrypted_password
+  end
+  
+  def self.authenticate(email, password)
+    u = find_by_email(email)
+    return nil if u.nil?
+    return u if u.has_password?(password)
+  end
+  
+  private
+  
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+  
+  def encrypt(password)
+    secure_hash("#{salt}--#{password}")
+  end
+    
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
   
 end
