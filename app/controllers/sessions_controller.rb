14090a9f9415b54deca961c92d71e9ef11f5dc6a
Aditya Sanghi <asanghi@me.com>
2010-09-01 13:44:35 +0530

    Done with user edit/update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     else
       sign_in user
       flash[:success] = "Welcome to the Sample App"
-      redirect_to user
+      redirect_back_or user
     end
   end
   
